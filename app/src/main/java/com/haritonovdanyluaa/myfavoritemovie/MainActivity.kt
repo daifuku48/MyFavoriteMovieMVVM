988bf1900a299b394fb558b49df1fb6fbbbd2024
danil <natalya.haritonov.72@gmail.com>
2023-04-29 21:55:07 +0300

    ui

--- app/src/main/java/com/haritonovdanyluaa/myfavoritemovie/MainActivity.kt
+++ app/src/main/java/com/haritonovdanyluaa/myfavoritemovie/MainActivity.kt
@@ -2,10 +2,27 @@ package com.haritonovdanyluaa.myfavoritemovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haritonovdanyluaa.myfavoritemovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+    private var viewPagerAdapter: ViewPagerAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding?.tabLayout?.addTab(binding?.tabLayout?.newTab()!!.setText("Favorites"))
+        binding?.tabLayout?.addTab(binding?.tabLayout?.newTab()!!.setText("Movies"))
+
+        val fragmentManager = supportFragmentManager
+        viewPagerAdapter = ViewPagerAdapter(fragmentManager, lifecycle)
+        binding?.viewPager?.adapter = viewPagerAdapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+        viewPagerAdapter = null
     }
 }
